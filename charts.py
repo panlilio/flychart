177ebae4df989f57138d87bf3b855fc215daee65
Mia Panlilio <mia.panlilio@gmail.com>
2024-04-11 17:01:01 -0500

    classes defined

--- charts.py
+++ charts.py
@@ -8,92 +8,167 @@ class Volume:
         self.data_segmented = data_segmented
         self.projection_type = projection_type
         self.intensity_method = intensity_method
-
+        
+        self._extractor = None
+        
     def chart(self):
         for z,(im,seg) in enumerate(zip(self.data_intensity,self.data_segmented)):
             sl = Slice(im,seg,intensity_method=self.intensity_method)
             f,s = sl.process()
-            
+
+    def extract_slices(self,z):
+        for vol in [self.data_intensity,self.data_segmented]:
+            imtype = itk.template(vol)
+            eif = itk.ExtractImageFilter.New(vol)
+            eif.SetDirectionCollapseToSubmatrix()
+            ir = vol.GetBufferedRegion()
+            sz = ir.GetSize()
+            sz[2] = 1
+            start = ir.GetIndex()
+   
+    @staticmethod
+    def _slice_extractor(data):
+        eif = itk.ExtractImageFilter.New(data)
+        eif.SetDirectionCollapseToSubmatrix()
+        ir = data.GetBufferedRegion()
+        sz = ir.GetSize()
+        sz[2] = 1
+        start = ir.GetIndex()
+        
+        def _slice_extractor_helper(z):
+            start[2] = z
+            tr = ir
+            tr.SetSize(sz)
+            tr.SetIndex(start)
+            eif.SetExtractionRegion(tr)
+            eif.Update()
+            return eif.GetOutput()
+
+        return _slice_extractor_helper
+
 class Slice:
-    def __init__(self,intensity_im,segmented_im,intensity_method="mean"):
-        self.intensity_im = intensity_im
-        self.segmented_im = segmented_im
+    def __init__(self,im_intensity,im_segmented,intensity_method="mean"):
+        self.im_intensity = im_intensity
+        self.im_segmented = im_segmented
         self.intensity_method = intensity_method
 
     def process(self):
         pass
 
+    def prepocess(self):
+        pass
+
+    def get_boundary(self):
+        imtype = itk.template(self.im_segmented)
+        cte = itk.ContourExtractor2DImageFilter[imtype].New(im_segmented)
+        cte.Update()
+        contours = cte.GetOutput()
+        contour = contours.GetContour(0)
+        x,y = contour.GetVertices()
+        return x,y
+
 class Boundary:
-    def __init__(self,x=None,y=None,dr=5,dtheta=5,eps=1e-6):
+    def __init__(self,x=(),y=(),dr=2,dtheta=2,eps=1e-6,di=2):
         self.x = x
         self.y = y
         self.dr = dr
         self.dtheta = dtheta
         self.eps = eps # Tolerance for checking if a number is zero 
+        self.di = di # \pm di points to use around the point of interest for tangent calculation
+        self.n = len(self.x)
 
     def process(self):
-        pass
-    
-    def get_arclength(self):
-        arclength = np.zeros(len(self.x))
-        for i in range(1,len(self.x)):
-            arclength[i] = arclength[i-1] + np.sqrt((self.x[i]-self.x[i-1])**2 + (self.y[i]-self.y[i-1])**2)
-        return arclength
-    
-    def generate_wedge_points(self):
-        midpt = int(len(self.x)/2)
-        x0 = self.x[midpt]
-        y0 = self.y[midpt]
+        S = self.arclength()
+        wedge_pts = []
+        for i in range(self.n):;
+            idx = self.get_circ_idx(i)
+            xs = self.x[idx]
+            ys = self.y[idx]
+            wedge_pts.append(self.generate_wedge_points(xs,ys,self.dr,self.dtheta,self.eps))
+        return S, wedge_pts
         
-        if len(x)==2:
-            dx = x[1]-x[0]
-            dy = y[1]-y[0]
+    def get_circ_idx(self,i):
+        idx = []
+        for j in range(i-self.di,i+self.di+1):
+            idx.append(j % self.n)
+        return idx
+    
+    def arclength(self):
+        return self.xy_to_arclength(self.x,self.y)
+
+    @staticmethod
+    def xy_to_arclength(x_,y_):
+        arclength = np.zeros(len(x_))
+        for i in range(1,len(x_)):
+            arclength[i] = arclength[i-1] + np.sqrt((x_[i]-x_[i-1])**2 + (y_[i]-y_[i-1])**2)
+        return arclength
+
+    @staticmethod
+    def generate_wedge_points(xs=None,ys=None,dr=2,dtheta=2,eps=1e-6):
+        midpt = int(len(xs)/2)
+        xmid = xs[midpt]
+        ymid = ys[midpt]
+        
+        if len(xs)==2:
+            dxs = xs[1]-xs[0]
+            dys = ys[1]-ys[0]
         else:
             # Use spline interpolation to calculate tangent
-            x = np.array(x)
-            y = np.array(y)
-            arclength = xy_to_arclength(x,y)
-            spx = sp.interpolate.UnivariateSpline(arclength,x)
-            spy = sp.interpolate.UnivariateSpline(arclength,y)
-            dx = spx.derivative()(arclength[midpt])
-            dy = spy.derivative()(arclength[midpt])
+            xs = np.array(xs)
+            ys = np.array(ys)
+            s = xy_to_arclength(xs,ys)
+            spx = sp.interpolate.UnivariateSpline(arclength,xs)
+            spy = sp.interpolate.UnivariateSpline(arclength,ys)
+            dxs = spx.derivative()(arclength[midpt])
+            dys = spy.derivative()(arclength[midpt])
 
-        norm = np.sqrt(dx**2 + dy**2)
-        dx = dx/norm
-        dy = dy/norm
+        norm = np.sqrt(dxs**2 + dys**2)
+        dxs = dxs/norm
+        dys = dys/norm
         
         # Coefficients of the normal line: c11*x + c12*y + c00 = 0
-        c11 = -dy
-        c12 = dx
+        c11 = -dys
+        c12 = dxs
         c00 = -x0*c11 - y0*c12
 
-        # Get polar coordinates from new coordinate system where the normal line passes through the origin
-        if np.abs(c11) < eps:
-            # Line is horizontal
-            x_ = x0
-            y_ = y0 - c00/c12
-        elif np.abs(c12) < eps:
-            # Line is vertical
-            y_ = y0
-            x_ = x0 - c00/c11
+        # Origin of second coordinate system translated so that the normal passes through (0,0)
+        if np.abs(c12) < eps: # Line is vertical
+            x0 = -c00/c11
+            y0 = 0
         else:
-            y_ = 0
-            x_ = -c00/c11
+            x0 = 0
+            y0 = -c00/c12
         
+        x_ = xmid - x0
+        y_ = ymid - y0
+
+        # Polar coordinates of the midpoint in second coordinate system
         r_ = np.sqrt(x_**2 + y_**2)
         theta_ = np.arctan2(y_,x_)
+
+        # Get extents of wedge and query x_,y_ points to see if contained in wedge
+        theta_exts = theta_ + np.array([-dtheta/2,dtheta/2])
+        r_exts = r_ + np.array([-dr/2,dr/2])
+
+        x_exts = r_exts[:,None]*np.cos(theta_exts)[None,:]
+        y_exts = r_exts[:,None]*np.sin(theta_exts)[None,:]
         
-        def g(dr,dtheta=0,ndr=10,ndtheta=1):
-            R_ = np.linspace(r_-dr/2,r_+dr/2,ndr)
-            Th_ = np.linspace(theta_-dtheta/2,theta_+dtheta/2,ndtheta)
-            X_ = R_[:,None]*np.cos(Th_)[None,:]
-            Y_ = R_[:,None]*np.sin(Th_)[None,:]
-            X = X_ + x_
-            Y = Y_ + y_
-            return X,Y
+        xm, xM = int(x_exts.min()), int(x_exts.max())
+        ym, yM = int(y_exts.min()), int(y_exts.max())
 
-        return g
-
+        X_, Y_ = np.meshgrid(np.arange(xm,xM+1),np.arange(ym,yM+1))
+        X_, Y_ = X_.flatten(), Y_.flatten()
+        wedge_pts = []
+        for i in range(len(X_)):
+            xi = X_[i]
+            yi = Y_[i]
+            r = np.sqrt(xi**2 + yi**2)
+            theta = np.arctan2(yi,xi)
+            if r >= r_exts[0] and r <= r_exts[1] and theta >= theta_exts[0] and theta <= theta_exts[1]:
+                wedge_pts.append((int(xi+x0),int(yi+y0))) #wedge points in original coordinate system
+        
+        wedge_pts = np.array(wedge_pts)
+        return wedge_pts
         
     @staticmethod
     def hammer_projection(phi,lam):
