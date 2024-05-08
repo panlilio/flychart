2e2a7f1d6208dd60ca8fd405aecb75f6c6e0b2c0
Mia Panlilio <mia.panlilio@gmail.com>
2024-05-08 14:46:28 -0500

    translate to positive image space

--- preprocess.py
+++ preprocess.py
@@ -13,36 +13,61 @@ class Preprocess:
         
         self.imtype = itk.Image[itk.template(self.intensity_image)[1]]
         
-    def to_principal_axes(self):
-        imc = itk.ImageMomentsCalculator[self.imtype].New(self.intensity_image)
+    def to_principal_axes(self,use_mask=True):
+        if use_mask:
+            if not self.mask_image:
+                self.mask_image = self.segment(self.intensity_image)
+            imc = itk.ImageMomentsCalculator[self.imtype].New(self.mask_image)
+        else:
+            imc = itk.ImageMomentsCalculator[self.imtype].New(self.intensity_image)
         imc.Compute()
-        self.transform = imc.GetPhysicalAxesToPrincipalAxesTransform()
+        T0 = imc.GetPhysicalAxesToPrincipalAxesTransform()
+        
+        T1 = T0.Clone()
+        T1.SetTranslation([0,0,0])
+
+        l,w,h = self.intensity_image.GetLargestPossibleRegion().GetSize()
+        exts0 = np.meshgrid(range(l),range(w),range(h))
+        exts0 = np.array(exts).reshape(3,-1).T
+        
+        exts1 = []
+        for ext in exts0:
+            exts1.append(T1.TransformPoint(ext))
+        exts1 = np.array(exts1)
+        m = np.min(exts,axis=0)
+        M = np.max(exts,axis=0)
+        dims = M-m
+        dims = [int(d) for d in dims]
+
+        T2 = T0.Clone()
+        T2.SetTranslation([-int(i) for i in m])
+        self.transform = T2
       
-        #self.transform.SetCenter(imc.GetCenterOfGravity())
-        #self.transform.SetTranslation([0,0,0])
-        rsf = itk.ResampleImageFilter[self.imtype, self.imtype].New()
+        refim = self.mkreferenceim(dims)
+
+        rsf = itk.ResampleImageFilter[self.imtype,self.imtype].New()
         rsf.SetInput(self.intensity_image)
-        rsf.SetReferenceImage(self.intensity_image)
+        rsf.SetReferenceImage(refim)
         rsf.UseReferenceImageOn()
 
-        interp = itk.LinearInterpolateImageFunction[self.imtype, itk.D].New()
+        interp = itk.LinearInterpolateImageFunction[self.imtype,itk.D].New()
         rsf.SetInterpolator(interp)
         rsf.SetTransform(self.transform)
-
         rsf.Update()
+
         self.aligned_intensity_image = rsf.GetOutput()
+        self.aligned_mask_image = self.segment(self.aligned_intensity_image)
+    
+    def mkreferenceim(self,lwh):
+        im = self.imtype.New()
+        region = im.GetLargestPossibleRegion()
+        region.SetSize(lwh)
+        im.SetRegions(region)
+        im.Allocate()
+        return im
 
-        if self.mask_image:
-            rsf.SetInput(self.mask_image)
-            interp = itk.NearestNeighborInterpolateImageFunction[self.imtype,itk.D].New()
-            rsf.SetInterpolator(interp)
-            rsf.Update()
-            self.aligned_mask_image = rsf.GetOutput()
-        else:
-            self.segment_aligned()
-
-    def segment_aligned(self):
+    def segment(self,im):
         otsu = itk.OtsuThresholdImageFilter[self.imtype,self.imtype].New()
-        otsu.SetInput(self.aligned_intensity_image)
+        otsu.SetInput(im)
         otsu.Update()
-        self.aligned_mask_image = otsu.GetOutput()
+        return otsu.GetOutput()
