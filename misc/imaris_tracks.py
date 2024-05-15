8252d2be7af512825a1f06f95a7207f2324bd32f
mia <mia@panlilio.phd>
2024-05-15 14:40:17 -0500

    ims tracks detected for all t+nt

--- misc/imaris_tracks.py
+++ misc/imaris_tracks.py
@@ -3,22 +3,25 @@ import numpy as np
 import pickle
 
 class Tracks:
-    def __init__(self,imsfile):
+    def __init__(self,imsfile,point_set_id=0):
         self.imsfile = imsfile
+        self.point_set_id = point_set_id
         self.load()
 
     def load(self):
         with h5py.File(self.imsfile, 'r') as f:
-            spot_xyz = np.array(f["Scene8/Content/Points0/Spot"])
-            _spot_t = np.array([[s[0],s[1],s[2]] for s in f["Scene8/Content/Points0/SpotTimeOffset"]])
+            point_handle = f[f"Scene8/Content/Points{self.point_set_id}"]
+
+            spot_xyz = np.array(point_handle["Spot"])
+
+            _spot_t = np.array([[s[0],s[1],s[2]] for s in point_handle["SpotTimeOffset"]])
             _spot_uid = np.array([s[0] for s in spot_xyz])
             _spot_xyz = np.array([[s[1],s[2],s[3]] for s in spot_xyz])
-            _track_idx = np.array(f["Scene8/Content/Points0/Track0"])
-            _track_edges = np.array(f["Scene8/Content/Points0/TrackEdge0"])
-           
+            _track_idx = np.array(point_handle["Track0"])
+            _track_edges = np.array(point_handle["TrackEdge0"])
+
             # Build dictionaries using unique identifiers as keys: note that we need to explicitly access each element of
             # arrays in the h5 file because they are stored in an array format that cannot be spliced
-            self._scratch = np.array(f["Scene8/Content/Points0/TrackObject0"])
             self.spot_dict = { s : [xyz[0] ,xyz[1], xyz[2]] for s,xyz in zip(_spot_uid,_spot_xyz) }
             self.t_dict = { t[0] : [t[1],t[2]] for t in _spot_t }
             self.trackobj_dict = { ed[0] : ed[1] for ed in _track_edges }
@@ -35,23 +38,26 @@ class Tracks:
             self.volume_shape = f["DataSet/ResolutionLevel 0/TimePoint 0/Channel 0/Data"].shape[::-1]
             self.resolution = np.median( self.volume_shape_um / self.volume_shape ) #assume isotropic resolution
 
-    def build_tracks(self,t=0,nt=10,in_voxels=True):
+    def build_tracks_t(self,t=0,nt=10,in_voxels=True):
         # Get all spot uids at time t
-        idx = self.t_dict[t]
-        uids = self.spot_uid[idx[0]:idx[1]]
         tracks = {}
-        for u in uids:
-            tracks[u] = []
-            tracks[u].append(self.spot_dict[u])
-            uu = u
-            for i in range(nt):
-                if uu in self.trackobj_dict:
-                    uu = self.trackobj_dict[uu]
-                    xyz = self.spot_dict[uu]
-                    tracks[u].append(xyz)
-                    i += 1
-                else:
-                    break
+        uids = []
+        for tt in range(t,t+nt):
+            idx = self.t_dict[tt]
+            uids_t = self.spot_uid[idx[0]:idx[1]]
+            for u in uids_t:
+                if u not in uids:
+                    tracks[u] = []
+                    tracks[u].append(self.spot_dict[u])
+                    uu = u
+                    for i in range(t+nt-tt):
+                        if uu in self.trackobj_dict:
+                            uu = self.trackobj_dict[uu]
+                            xyz = self.spot_dict[uu]
+                            tracks[u].append(xyz)
+                            uids.append(u)
+                        else:
+                            break
        
         if in_voxels:
             for t in tracks:
@@ -69,6 +75,6 @@ class Tracks:
 
 if __name__=="__main__":
     T = Tracks("/research/sharedresources/cbi/data_exchange/tayl1grp/2024_lightsheet/2023-08-23/20230823_Full_Embryo.ims")
-    tracks = T.build_tracks(t=60,nt=10)
+    tracks = T.build_tracks_t(t=60,nt=18)
     with open("tracks.pkl","wb") as f:
         pickle.dump(tracks,f)
